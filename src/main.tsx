ce17645adcc0b16991d1bf79edcb596e156ef386
Neeharika <veluvoluneeharika@gmail.com>
2026-07-02 18:45:55 +0530

    Fix portfolio data path for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { createRoot } from "react-dom/client";
 import Template from "./template";
 import "./index.css";
 
-fetch('/portfolio-data.json')
-  .then(response => response.json())
-  .then(data => {
+fetch("./portfolio-data.json")
+  .then((response) => response.json())
+  .then((data) => {
     createRoot(document.getElementById("root")!).render(
-      <Template portfolioData={data} />,
+      <Template portfolioData={data} />
     );
   })
-  .catch(error => console.error('Error loading portfolio data:', error));
-  
+  .catch((error) => console.error("Error loading portfolio data:", error));
