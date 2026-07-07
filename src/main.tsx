5252163c306d0db70ad931b35975448192020cf3
Ashrit-Raghupatruni <ashritraghupatruni200407@gmail.com>
2026-07-07 22:28:02 +0530

    chore: change fetch to static import for portfolio data

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { createRoot } from "react-dom/client";
 import Template from "./template";
+import data from "../public/portfolio-data.json";
 import "./index.css";
 
-fetch("./portfolio-data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    createRoot(document.getElementById("root")!).render(
-      <Template portfolioData={data} />
-    );
-  })
-  .catch((error) => console.error("Error loading portfolio data:", error));
+createRoot(document.getElementById("root")!).render(
+  <Template portfolioData={data} />
+);
